3119e8a88fd0a36d8c375f2d8bc7bca82558f598
Abhij <abhinabajash48@gmail.com>
2024-06-18 09:55:23 +0530

    Nothing just a first commit

---
+++ test.js
@@ -0,0 +1,54 @@
+let object = {
+    "location": {
+        "name": "London",
+        "region": "City of London, Greater London",
+        "country": "United Kingdom",
+        "lat": 51.52,
+        "lon": -0.11,
+        "tz_id": "Europe/London",
+        "localtime_epoch": 1715525752,
+        "localtime": "2024-05-12 15:55"
+    },
+    "current": {
+        "last_updated_epoch": 1715525100,
+        "last_updated": "2024-05-12 15:45",
+        "temp_c": 26.0,
+        "temp_f": 78.8,
+        "is_day": 1,
+        "condition": {
+            "text": "Sunny",
+            "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+            "code": 1000
+        },
+        "wind_mph": 6.9,
+        "wind_kph": 11.2,
+        "wind_degree": 140,
+        "wind_dir": "SE",
+        "pressure_mb": 1010.0,
+        "pressure_in": 29.83,
+        "precip_mm": 0.02,
+        "precip_in": 0.0,
+        "humidity": 37,
+        "cloud": 0,
+        "feelslike_c": 26.6,
+        "feelslike_f": 79.9,
+        "vis_km": 10.0,
+        "vis_miles": 6.0,
+        "uv": 5.0,
+        "gust_mph": 10.4,
+        "gust_kph": 16.7,
+        "air_quality": {
+            "co": 217.0,
+            "no2": 8.5,
+            "o3": 117.3,
+            "so2": 5.7,
+            "pm2_5": 9.3,
+            "pm10": 10.1,
+            "us-epa-index": 1,
+            "gb-defra-index": 1
+        }
+    }
+}
+console.log(object);
+console.log("jsdhksh");
+console.log(object.location.name);
